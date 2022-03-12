8f73ab8486f4f1449e4e917f6cc8de7beeabe76d
gizem <gizemc.deniz@gmail.com>
2022-03-12 19:09:47 +0300

    done

--- src/index.js
+++ src/index.js
@@ -1,7 +1,17 @@
 // Note: createStore and candyReducer must be exported for the tests to run
 
-export function createStore() {
-  // write your createStore code here
+export function createStore(reducer) {
+  let state;
+  function dispatch(action){
+    state = reducer(state,action)
+    render()
+  }
+  function getState(){
+    return state;
+  }
+  return {
+    dispatch, getState
+  }
 }
 
 export function candyReducer(state = [], action) {
@@ -12,7 +22,7 @@ export function candyReducer(state = [], action) {
       return state;
   }
 }
-
+const store = createStore(candyReducer)
 function render() {
   let container = document.getElementById("container");
   if (store.getState()) {
@@ -24,3 +34,4 @@ function render() {
 
 // Use your createStore function and the functions provided here to create a store.
 // Once the store is created, call an initial dispatch.
+store.dispatch({type:"@@INIT"})
